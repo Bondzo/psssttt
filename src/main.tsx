d5891a5b2857b5882182d5fe2b3c45a760063407
Bondzo <satyanovario054@gmail.com>
2025-10-20 12:41:53 +0700

    Revert "Add theme provider with dark mode toggle"

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { CartProvider } from "@/components/cart-context";
-import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </ThemeProvider>
+  <CartProvider>
+    <App />
+  </CartProvider>
 );
